d3c8d9454cb34187b068cee71b3fa040735fa384
blockout22 <kie1k93@gmail.com>
2023-06-04 20:35:12 +0100

    Fixed some shadow issues

--- OpenGlRenderer/src/main/resources/shaders/DepthFragment.glsl
+++ OpenGlRenderer/src/main/resources/shaders/DepthFragment.glsl
@@ -15,7 +15,21 @@ float LinearizeDepth(float depth)
 void main(){
 
 //    FragDepth = gl_FragCoord.z;
-    FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+//    FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+    float val = gl_FragCoord.z;
+    if(val > 1){
+        val = 0.5;
+    }else if(val > 0){
+        val = 1;
+    }else{
+        val = 0;
+    }
+    FragColor.r = val;
+//    float z = gl_FragCoord.z;
+//    float w = gl_FragCoord.w;
+//    w = z / (1.0 - z);
+//    float linearDepth = z / w;
+//    FragColor.r = linearDepth;
 //    FragColor = vec4(vec3(LinearizeDepth(gl_FragCoord.z)), 1.0);
 
     // Convert depth value from [0,1] range to linear depth.
